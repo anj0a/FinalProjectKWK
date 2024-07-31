a0179fd293a1a7bdec63b08b00e69af689ef778b
Angelina <an.joa737@gmail.com>
2024-07-31 11:02:47 -0700

    Initial Commit

---
+++ FinalProjectP1/FinalProjectP1App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectP1App.swift
+//  FinalProjectP1
+//
+//  Created by Scholar on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectP1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
